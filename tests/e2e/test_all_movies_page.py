2e257c8f9442714d3499e8202fc55425c30e30f3
cbrow353 <cbrow353@uncc.edu>
2023-03-19 00:35:21 -0400

    finished unit testing as well

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,8 +1,8 @@
-# TODO: Feature 1
 import pytest
 from app import app
 from src.repositories.movie_repository import get_movie_repository
 
+# TODO: Feature 1 [DONE]
 def test_all_movies_page():
     test_app = app.test_client()
     response = test_app.get('/movies')
@@ -16,11 +16,3 @@ def test_show_movie():
     response = test_app.get('/movies')
     assert response.status_code == 200
     assert b'<td>The Matrix</td>' in response.data
-
-def test_app_function():
-    # test the app.py list_all_movies function
-    test_app = app.test_client()
-    movie_repository = get_movie_repository()
-    assert len(movie_repository.get_all_movies()) == 1
-    movie_repository.create_movie('Sample movie', 'Bob Sample', 1)
-    assert len(movie_repository.get_all_movies()) == 2
