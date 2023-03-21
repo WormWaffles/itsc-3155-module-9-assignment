ec9707396c97d363eb4ead2cbcdc0944d706b191
Cameron Jackson <c737jackson@gmail.com>
2023-03-21 19:49:37 -0400

    Going to update to the most reason

--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,6 +1,8 @@
 from flask.testing import FlaskClient
+from app import app
 # TODO: Feature 3
 
 @pytest.fixture()
 def test_search():
+    test_app = app.test_client()
     response = test_app.get('/movies/search')
