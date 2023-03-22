cd4bc976a9babd4a0aaa6834f64deb60124585b3
Jai_Vang <jaivang0387@gmail.com>
2023-03-22 14:38:50 -0400

    complete feature-4

--- app.py
+++ app.py
@@ -61,7 +61,8 @@ def get_single_movie(movie_id: int):
 
 @app.get('/movies/<int:movie_id>/edit')
 def get_edit_movies_page(movie_id: int):
-    return render_template('edit_movies_form.html')
+    movie = movie_repository.get_movie_by_id(movie_id)
+    return render_template('edit_movies_form.html', movie=movie)
 
 
 @app.post('/movies/<int:movie_id>')
