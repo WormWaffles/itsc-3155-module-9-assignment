fb34b382bf60e5a36987ffdbc9eeda6d0fb0cb67
cbrow353 <cbrow353@uncc.edu>
2023-03-22 11:04:06 -0400

    changed some tests

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -8,11 +8,10 @@ def test_all_movies_page():
     response = test_app.get('/movies')
     assert response.status_code == 200
     assert b'<td colspan="3">No movies found</td>' in response.data
-
-def test_show_movie():
-    test_app = app.test_client()
     movie_repository = get_movie_repository()
     movie_repository.create_movie('The Matrix', 'Wachowski', 5)
     response = test_app.get('/movies')
     assert response.status_code == 200
     assert b'<td>The Matrix</td>' in response.data
+    assert b'<td>Wachowski</td>' in response.data
+    assert b'<td>5/5</td>' in response.data
