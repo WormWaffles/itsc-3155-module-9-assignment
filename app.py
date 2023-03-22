103333c167f8575ac01dd6c9add94b842b0c8129
Jai_Vang <jaivang0387@gmail.com>
2023-03-22 08:50:58 -0400

    my first commit (Testing)

--- app.py
+++ app.py
@@ -52,7 +52,10 @@ def search_movies():
 
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
-    # TODO: Feature 4
+    # TODO: Feature 4....
+
+
+
     return render_template('get_single_movie.html')
 
 
