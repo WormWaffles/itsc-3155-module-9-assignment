287ccf6bad87877995df078f81213793c3d16ebd
cbrow353 <cbrow353@uncc.edu>
2023-03-22 11:18:38 -0400

    Added one line

--- tests/e2e/test_index.py
+++ tests/e2e/test_index.py
@@ -4,5 +4,5 @@ from flask.testing import FlaskClient
 def test_home_page(test_app: FlaskClient):
     response = test_app.get('/')
     response_data = response.data.decode('utf-8')
-
+    assert response.status_code == 200
     assert '<h1 class="mb-5">Movie Rating App</h1>' in response_data
