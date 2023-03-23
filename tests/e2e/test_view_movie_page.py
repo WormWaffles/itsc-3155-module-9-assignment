4d654b2078298534b127045fcffecc54ff1e92e9
Jai_Vang <jaivang0387@gmail.com>
2023-03-22 22:57:56 -0400

    complete feature-4 e2e test

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -3,12 +3,13 @@ from src.repositories.movie_repository import get_movie_repository
 
 # TODO: Feature 4
 def test_get_single_movie():
-    test_app = app.test_client()
-
-    data = response.data.decode('utf-8')
-    response =  test_app.get('/movies/search')
+    movie_repository = get_movie_repository()
+    movie_repository.clear_db()
+    movie1 = movie_repository.create_movie('Chef', 'Jon Favreau', 3)
+    tester = app.test_client()
+    response = tester.get(f'/movies/{movie1.movie_id}/edit', data={'action': 'clicked'})
     assert response.status_code == 200
+    assert b'<label for="title" class="form-label">Movie Name</label>' in response.data
     
-
-    ##Test that page is rendered
+    
     
