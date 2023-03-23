bc86bee047d171baeff7e390d4194d9840938d51
Jai_Vang <jaivang0387@gmail.com>
2023-03-22 21:30:58 -0400

    adding unit test for feature-4

--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,1 +1,24 @@
+from app import app
+from src.repositories.movie_repository import get_movie_repository
+
 # TODO: Feature 4
+
+def test_create_movie():
+    movie_repository = get_movie_repository()
+    movie_repository.clear_db()
+
+    #create movie object
+    movie1 = movie_repository.create_movie('Transformer', 'Mike Bay', 2)
+
+    #Create new movie object by using get_movie_by_id function
+    # with the movie object created by create_movie function
+    movie2 = movie_repository.get_movie_by_id(movie1.movie_id)
+
+    #compare movie.id == movie2.id
+    assert movie1.movie_id == movie2.movie_id
+
+    #obtain movie object by using get_movie_by_id function from a movie that doesn't exist
+    movie2 = movie_repository.get_movie_by_id(12345)
+    assert  movie2 == None
+
+
